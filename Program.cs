7d2dcfa5ec032213ec0fc5763028bdf0b93ca23a
nick <nmcamp87@gmail.com>
2021-10-05 20:07:36 -0400

    Solution and project added

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace P4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
