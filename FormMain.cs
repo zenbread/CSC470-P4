65b66b15fa057da0b030f3e736cf32b46db43d4e
nick <nmcamp87@gmail.com>
2021-10-05 20:07:23 -0400

    Main form updated with look and code

---
+++ FormMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P4
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+            FormLogin formLogin = new FormLogin();
+            DialogResult res;
+            AppUser appUser;
+            try
+            {
+                do
+                {
+                    res = formLogin.ShowDialog();
+                    appUser = formLogin.ReturnUser;
+                }
+                while (!appUser.isAuthenticated && res == DialogResult.OK);
+
+                if (res != DialogResult.OK)
+                {
+                    this.Close();
+                }
+            }
+            catch (NullReferenceException) {
+                this.Close();
+            }
+            SetText();
+        }
+
+        private void SetText()
+        {
+            Text = "Main - No Project Selected";
+        }
+
+    }
+}
